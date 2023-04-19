bfe081fb9e3f2219a467ea9b39236ae4eae4d6b8
Nathan Gagne <nathangagne.dev@gmail.com>
2023-04-19 11:44:18 -0500

    Update CraneMovement for rotation Update CraneMovement such that the vehicle doesn't move from side to side without rotating the treads. 'a' and 'd' rotate the treads and 'w' and 's' move in the direction.

--- Assets/Scripts/CraneMovement.cs
+++ Assets/Scripts/CraneMovement.cs
@@ -18,7 +18,7 @@ public class CraneMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
-        float moveX = 0.0f; //default is to not move
+        int rotateY = 0; //degrees to rotate per frame around Y axis. 0 by default
         float moveZ = 0.0f;
 
         //MotorShake calculations. This can also be accomplished with InvokeRepeating(...)
@@ -34,16 +34,17 @@ public class CraneMovement : MonoBehaviour
         float inputZ = Input.GetAxisRaw("Vertical");
 
         if (inputX > 0)
-            moveX = 0.01f;
+            rotateY = 1;
         else if (inputX < 0)
-            moveX = -0.01f;
+            rotateY = -1;
 
         if (inputZ > 0)
             moveZ = 0.01f;
         else if (inputZ < 0)
             moveZ = -0.01f;
 
-        transform.Translate(moveX,motorShakeY,moveZ);  
+        transform.Rotate(0,rotateY, 0);
+        transform.Translate(0,motorShakeY,moveZ);  
     }
 
 }
